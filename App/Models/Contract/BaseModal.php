d4a74e31d05757e2fdeb3e965555eda871beeff1
mr_mn2 <mjfy1382@gmail.com>
2022-06-30 15:55:08 +0430

    some bugs fixed

--- App/Models/Contract/BaseModal.php
+++ App/Models/Contract/BaseModal.php
@@ -18,19 +18,21 @@ abstract class BaseModal extends Crudinterface{
         return (object)[];
     }
     public function get(array $culums,array $where):array{
+        return [];
 
     }
     public function update(array $data,array $where):int{
-
+        return 1;
     }
     public function delete(array $where):int
     {
-
+        return 1;
     }
     protected function get_attributes($key){
         if(!$key || !array_key_exists($key,$this->attributes)){
             return null;
         }
+        
         return $this->attributes[$key];
 
     }
